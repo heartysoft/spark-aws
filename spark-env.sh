5c1d92f19362b15a1cb4e07a8ea93cd137ef16f7
Luis Vicente <langel.vicente@gmail.com>
2017-05-09 10:59:36 +0100

    Make recovery path configurable.

--- spark-env.sh
+++ spark-env.sh
@@ -1,5 +1,8 @@
 SPARK_NO_DAEMONIZE=true
 
-if [ -n "$ZOOKEEPER_URL" ]; then 
-	export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.dir=/spark -Dspark.deploy.zookeeper.url=$ZOOKEEPER_URL"
+if [ -n "$ZOOKEEPER_URL" ] && [ "$ZOOKEEPER_PATH" ]; then 
+	export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.dir=$ZOOKEEPER_PATH -Dspark.deploy.zookeeper.url=$ZOOKEEPER_URL"
+elif [ -n "$ZOOKEEPER_URL" ] || [ "$ZOOKEEPER_PATH" ]; then
+	echo "Both ZOOKEEPER_URL and ZOOKEEPER_PATH should be set."
+	exit 1
 fi
